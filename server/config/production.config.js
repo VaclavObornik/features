003722ee7bd03fa74baa7eb439e51ce171982513
JAN VOJACEK <vojacekjan@hotmail.com>
2017-06-06 16:26:38 +0200

    MongoDB connection update.

--- server/config/production.config.js
+++ server/config/production.config.js
@@ -7,21 +7,16 @@ module.exports = {
     production: true,
 
     db: {
-        url: process.env.DATABASE_SERVICE_NAME || null,
-        options: {
-            user: process.env.MONGODB_USER || null,
-            password: process.env.MONGODB_PASSWORD || null
-        }
+        url: process.env.MONGODB_URI || null,
+        options: {}
     },
 
     logger: {
         transports: [
             {
                 transport: 'MongoDB',
-                db: process.env.DATABASE_SERVICE_NAME || null,
+                db: process.env.MONGODB_URI || null,
                 collection: 'logs',
-                username: process.env.MONGODB_USER || null,
-                password: process.env.MONGODB_PASSWORD || null,
                 capped: true,
                 cappedSize: 400000000
             }
